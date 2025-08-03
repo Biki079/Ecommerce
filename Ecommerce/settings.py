be0ef0b7115efa27442d9e85ced9320659839d76
Biki079 <bikashkc2057@gmail.com>
2025-08-03 22:55:55 +0545

    Update category model and project files

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -125,6 +125,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'Ecommerce' / 'static',
 ]
 
+# mediafiles
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
